0a18850d3f94a7c033ac26e2284d2eaacf3c2a98
Eduardo-Lazaro <evandrolazarosospfc@gmail.com>
2021-10-26 21:41:44 -0300

    fire base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: 'AIzaSyD5ZBbNxc0TPE41al8vpNWgCV3CxDO22tU',
+    authDomain: 'my-academy-6e065.firebaseapp.com',
+    projectId: 'my-academy-6e065',
+    storageBucket: 'my-academy-6e065.appspot.com',
+    messagingSenderId: '617287675057',
+    appId: '1:617287675057:web:9dcdaf452cbf31d8e44690'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: 'AIzaSyD5ZBbNxc0TPE41al8vpNWgCV3CxDO22tU',
+    authDomain: 'my-academy-6e065.firebaseapp.com',
+    projectId: 'my-academy-6e065',
+    storageBucket: 'my-academy-6e065.appspot.com',
+    messagingSenderId: '617287675057',
+    appId: '1:617287675057:web:9dcdaf452cbf31d8e44690'
+  },
 };
 
 /*
